a4749382976c2685872d767d7d93b6684859094e
Timofei1998 <mironovtim@mail.ru>
2024-09-25 12:05:33 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,10 @@
+name = 'John'
+print('Name: ' + name)
+age = 25
+print('Age:', age)
+age = age+1
+new_age = 26
+print('New age:', age)
+# is_student= age<age+1
+is_student = new_age == age
+print('Is_student:', is_student)
